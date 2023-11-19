60c3238e71117a856d406575474b3e5b04ac1b33
Hamzahk4 <110351211+Hamzahk4@users.noreply.github.com>
2023-11-19 10:40:28 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,59 +3,22 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         Algorithm Alg = new Algorithm();
-//        System.out.println(Alg.digits(23469236));
-//        System.out.println(Alg.digits(1111));
-//        System.out.println(Alg.digits(98));
-//        System.out.println();
-//        System.out.println(Alg.divisible(9, 4));
-//        System.out.println(Alg.divisible(9, 3));
-//        System.out.println(Alg.divisible(100, 10));
-//        System.out.println();
-//        System.out.println(Alg.sumOfAllDigits(999));
-//        System.out.println(Alg.sumOfAllDigits(102837));
-//        System.out.println(Alg.sumOfAllDigits(1245));
-//        System.out.println();
-//        System.out.println(Alg.sumOfAllInBetween(1,3));
-//        System.out.println(Alg.sumOfAllInBetween(3,7));
-//        System.out.println(Alg.sumOfAllInBetween(4,974));
-//        System.out.println();
-//        System.out.println(Alg.multiplesOfTen(10,99));
-//        System.out.println(Alg.multiplesOfTen(5,27));
-//        System.out.println(Alg.multiplesOfTen(101,990));
-//        System.out.println();
-//        System.out.println(Alg.powerOfTwo(800));
-//        System.out.println(Alg.powerOfTwo(2));
-//        System.out.println(Alg.powerOfTwo(15));
-//          System.out.println();
-
-        /// STRINGS ///
-
-//        System.out.println(Alg.sameString("Hello World", "World"));
-//        System.out.println(Alg.sameString("Hello World", "cat"));
-//        System.out.println(Alg.sameString("Hello World", "orl"));
-//        System.out.println();
-//        System.out.println(Alg.reverse("gohangasalami"));
-//        System.out.println(Alg.reverse("moo"));
-//        System.out.println(Alg.reverse("Messisthegoat"));
-//        System.out.println();
-//        System.out.println(Alg.palindrome("amanaplanacanalpanama"));
-//        System.out.println(Alg.palindrome("Messibetter"));
-//        System.out.println(Alg.palindrome("racecar"));
-//        System.out.println();
-//        System.out.println(Alg.numOccurances("Ratatatatatat", "at"));
-//        System.out.println(Alg.numOccurances("Mississippi", "si"));
-//        System.out.println(Alg.numOccurances("Messisimplybetter", "wheel"));
-//        System.out.println();
-
-
-
-
-
-
-
+        System.out.println(Alg.sameString("Hello World", "World"));
+        System.out.println(Alg.sameString("Hello World", "cat"));
+        System.out.println(Alg.sameString("Hello World", "orl"));
+        System.out.println();
+        System.out.println(Alg.reverse("gohangasalami"));
+        System.out.println(Alg.reverse("moo"));
+        System.out.println(Alg.reverse("Messisthegoat"));
+        System.out.println();
+        System.out.println(Alg.palindrome("amanaplanacanalpanama"));
+        System.out.println(Alg.palindrome("Messibetter"));
+        System.out.println(Alg.palindrome("racecar"));
+        System.out.println();
+        System.out.println(Alg.numOccurances("Ratatatatatat", "at"));
+        System.out.println(Alg.numOccurances("Mississippi", "si"));
+        System.out.println(Alg.numOccurances("Messisimplybetter", "wheel"));
+        System.out.println();
     }
-
-
-
     }
 
