5346aae8e3c87eaef20de5dcd31816cee974df5b
HamzahK <hamzahk4@nycstudents.net>
2024-02-13 04:13:41 -0500

    n

--- Algorithm.java
+++ Algorithm.java
@@ -1,262 +1,43 @@
-import java.sql.SQLOutput;
+import java.util.ArrayList;
 
 public class Algorithm {
-    public int digits(int i) { //HOW MANY DIGITS
-        String temp = " " + i;
-        int numDigits = temp.length() - 1;
-        return numDigits;
-    }
 
-    public boolean divisible(int a, int b) { //ARE THEY DIVISIBLE?
-        return ((a % b) == 0);
-    }
-
-    public int sumOfAllDigits(int a) { // 2902
-        String number = a + "";
-        int numLength = number.length();
-        int sum = 0;
-        int i = 0;
-//        System.out.println(number);
-        while (i < numLength) {
-            String stringatpoint = number.substring(i, i + 1);
-            int numatpoint = Integer.parseInt(stringatpoint);
-            sum += numatpoint;
-            i++;
+    public int lessThanZero(int[] arr){
+        int count = 0;
+        for (int i : arr){
+            if(i<0) count++;
         }
-        return sum;
+        return count;
     }
 
-    public int sumOfAllInBetween(int num1, int num2) {
-        int i = 0;//3
-        int sum = 0;
-        while (i + num1 <= num2) {
-            if ((num1 + i) % 2 != 0) {
-                sum += (num1 + i);
-            }
-            i++;
+    public boolean ifFound(String[] str, String s){
+        boolean bool = false;
+        for (String i : str){
+            if (i.equals(s) == true) bool = true;
         }
-        return sum;
+        return bool;
     }
 
-    public int multiplesOfTen(int num1, int num2) {
-        int start;
-        int sum = 0;
-        if (num1 % 10 == 0) { //is num1 a multiple?
-            start = num1;
-        } else {
-            start = num1 + (10 - num1 % 10); // if not, it will be the closest multiple
-        }
-        while (start <= num2) {
-            sum += start;
-            start += 10;
-        }
-        return sum;
-    }
-
-    public int powerOfTwo(int num) { // had to figure out what the question was even asking to be done***
-        int i = 1;
-        while (i * 2 <= num) {
-            i *= 2;
-        }
-
-        return i;
-    }
-
-    ////// STRINGS
-    public boolean sameString(String a, String b) {
-        return (a.contains(b));
-    }
-
-    public String reverse(String str) {
-        String newString = "";
-        int i = str.length();
-        ;
-        while (i > 0) {
-            newString += str.substring(i - 1, i);
-            i--;
-        }
-        return newString;
-    }
-
-    public boolean palindrome(String str) {
-        String newString = "";
-        int i = str.length();
-        ;
-        while (i > 0) {
-            newString += str.substring(i - 1, i);
-            i--;
-        }
-        return (str.equals(newString));
-    }
-
-    public int numOccurances(String a, String b) {
-        int strcount = 0;
-        int index = a.indexOf(b);
-        while (index != -1) {
-            strcount++;
-            index = a.indexOf(b, index + 1);
-        }
-        return strcount;
-    }
-
-
-    //ARRAYYYSSSSSS
-    public void printArray(int[] array) {
-        for (int i : array) {
-            System.out.println(i);
-        }
-    }
-
-    public int maxOfArray(int[] array) {
-        int greatest = array[0];
-        for (int i : array) {
-            if (i > greatest) {
-                greatest = i;
+    public boolean inBoth (ArrayList<Integer> a,ArrayList<Integer> b){
+        for (int i : a){
+            for (int j : b){
+                if(i == j) return true;
             }
         }
-        return greatest;
+        return false;
     }
 
-    public int minOfArray(int[] array) {
-        int least = array[0];
-        for (int i : array) {
-            if (i < least) {
-                least = i;
-            }
-        }
-        return least;
-    }
-
-    public void swapArray(int[] array, int a, int b) {/// 1,2,3,4   1   2
-        int temp = array[a];
-        array[a] = array[b];
-        array[b] = temp;
-        for (int i : array) {
-            System.out.println(i);
-        }
-        System.out.println();
-    }
-
-    public String minOfStringArray(String[] array) {
-        String least = array[0];
-        for (String i : array) {
-            if (i.length() < least.length()) {
-                least = i;
-            }
-        }
-        return least;
-    }
-
-    public String maxOfStringArray(String[] array) {
-        String greatest = array[0];
-        for (String i : array) {
-            if (i.length() > greatest.length()) {
-                greatest = i;
-            }
-        }
-        return greatest;
-    }
-
-    public int largestDif(int[] a, int[] b) {
-        int alargest = 0;
-        int blargest = 0;
-        for (int i : a) {
-            if (Math.abs(i) > alargest) {
-                alargest = Math.abs(i);
-            }
-        }
-        for (int i : b) {
-            if (Math.abs(i) > blargest) {
-                blargest = Math.abs(i);
-            }
-        }
-        return blargest + alargest;
-    }
-
-
-    public double meanAvg(double[] array) {
-        double sum = 0;
-        for (double i : array) {
-            sum = sum + i;
-        }
-        return sum / array.length;
-    }
-
-    public void reverseArray(int[] array) {
-        int firstVal = 0;
-        int lastVal = array.length - 1;
-
-        while (firstVal < lastVal) {
-            int temp = array[firstVal];
-            array[firstVal] = array[lastVal];
-            array[lastVal] = temp;
-            firstVal = firstVal + 1;
-            lastVal = lastVal - 1;
-        }
-        for (int i : array) {
-            System.out.println(i);
-        }
-        ;
-        System.out.println();
-    }
-
-
-    public int firstNeg(int[] array) {
-        for (int i : array) {
-            if (i < 0) return i;
+    public int firstIndexList(ArrayList<Integer> arr, int a){
+        for(int i = 0;i < arr.size(); i++){
+            if (arr.get(i) == a) return i;
         }
         return -1;
     }
 
-    public boolean twoConsecutiveVals(int[] array) {
-        int i = 0;
-        while (i < array.length - 1) {
-            if (array[i] == array[i + 1]) {
-                return true;
-            }
-            i++;
+    public int firstIndexArray(int[] arr, int a){
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == a) return i;
         }
-        return false;
+        return -1;
     }
-
-    public static boolean equivalantVals(int[] array) {
-        int i = 0;
-        while (i < array.length - 1) {
-            int j = i + 1;
-            while (j < array.length) {
-                if (array[i] == array[j]) {
-                    return true;
-                    // if one value is = to other values of the index
-                }
-                j++;
-            }
-            // if not, move on to the next value and check
-            i++;
-        }
-        return false;
-    }
-
-    public int longestStreak(int[] array, int targetVal){
-        int streak = 0;
-        int maxStreak = 0;
-        for (int i : array){
-            if (i == targetVal){
-                streak++;
-                if (streak > maxStreak){maxStreak = streak;}
-            }
-            else streak = 0; //reset
-        }
-        return maxStreak;
-    }
-
-    public String alphabeticallyLast(String[] array) {
-        String lastWordAlphabetically = array[0]; // assume the first one is the last one
-        int i = 1;
-        while (i < array.length) {
-            if (array[i].compareTo(lastWordAlphabetically) > 0) {
-                lastWordAlphabetically = array[i];}
-            i++;}
-        return lastWordAlphabetically;
-    }
-
 }
--- Main.java
+++ Main.java
@@ -1,123 +1,30 @@
-import java.sql.SQLOutput;
+import com.sun.org.apache.xml.internal.security.algorithms.JCEMapper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Algorithm Alg = new Algorithm();
-//        System.out.println(Alg.digits(23469236));
-//        System.out.println(Alg.digits(1111));
-//        System.out.println(Alg.digits(98));
-//        System.out.println();
-//        System.out.println(Alg.divisible(9, 4));
-//        System.out.println(Alg.divisible(9, 3));
-//        System.out.println(Alg.divisible(100, 10));
-//        System.out.println();
-//        System.out.println(Alg.sumOfAllDigits(999));
-//        System.out.println(Alg.sumOfAllDigits(102837));
-//        System.out.println(Alg.sumOfAllDigits(12995));
-//        System.out.println();
-//        System.out.println(Alg.sumOfAllInBetween(1,3));
-//        System.out.println(Alg.sumOfAllInBetween(3,7));
-//        System.out.println(Alg.sumOfAllInBetween(4,974));
-//        System.out.println();
-//        System.out.println(Alg.multiplesOfTen(10,99));
-//        System.out.println(Alg.multiplesOfTen(5,27));
-//        System.out.println(Alg.multiplesOfTen(101,990));
-//        System.out.println();
-//        System.out.println(Alg.powerOfTwo(800));
-//        System.out.println(Alg.powerOfTwo(2));
-//        System.out.println(Alg.powerOfTwo(15));
-//          System.out.println();
-
-        /// STRINGS ///
-
-//        System.out.println(Alg.sameString("Hello World", "World"));
-//        System.out.println(Alg.sameString("Hello World", "cat"));
-//        System.out.println(Alg.sameString("Hello World", "orl"));
-//        System.out.println();
-//        System.out.println(Alg.reverse("gohangasalami"));
-//        System.out.println(Alg.reverse("moo"));
-//        System.out.println(Alg.reverse("Messisthegoat"));
-//        System.out.println();
-//        System.out.println(Alg.palindrome("amanaplanacanalpanama"));
-//        System.out.println(Alg.palindrome("Messibetter"));
-//        System.out.println(Alg.palindrome("racecar"));
-//        System.out.println();
-//        System.out.println(Alg.numOccurances("Ratatatatatat", "at"));
-//        System.out.println(Alg.numOccurances("Mississippi", "si"));
-//        System.out.println(Alg.numOccurances("Messisimplybetter", "wheel"));
-//        System.out.println()
-
-        // ARRAYYYSSSSSSS *some of them print just to verify that it worked*
-        Alg.printArray(new int[]{1,2,3});
-        Alg.printArray(new int[]{1,1,1,1,1,1,1,4});
-        Alg.printArray(new int[]{6, 2,5});
-        System.out.println();
-        Alg.swapArray(new int[]{1,2,3,4},1,2);
-        Alg.swapArray(new int[]{1,2,3,4,5},2,1);
-        Alg.swapArray(new int[]{99,81,33},0,2); //prints to check if it worked*
-        System.out.println();
-        System.out.println(Alg.maxOfArray(new int[]{3,2,1}));
-        System.out.println(Alg.maxOfArray(new int[]{1000000,443,2}));
-        System.out.println(Alg.maxOfArray(new int[]{2,2,2,2,2,2,1562}));
-        System.out.println();
-        System.out.println(Alg.minOfArray(new int[]{3,2,1}));
-        System.out.println(Alg.minOfArray(new int[]{1000000,443,89}));
-        System.out.println(Alg.minOfArray(new int[]{2,1,2,2,2,2,1562}));
-        System.out.println();
-        System.out.println(Alg.minOfStringArray(new String[]{"Hello,", "world", "!"}));
-        System.out.println(Alg.minOfStringArray(new String[]{"Messi","is","betterthanronaldo"}));
-        System.out.println(Alg.minOfStringArray(new String[]{"Mr.Holmer","W","teacher"}));
-        System.out.println();
-        System.out.println(Alg.maxOfStringArray(new String[]{"Hello,", "world", "!"}));
-        System.out.println(Alg.maxOfStringArray(new String[]{"Messi","is","the","GOAT"}));
-        System.out.println(Alg.maxOfStringArray(new String[]{"Let","him","cook"}));
-        System.out.println();
-        System.out.println(Alg.meanAvg(new double[]{2.0,3.0,4.0,5.0}));
-        System.out.println(Alg.meanAvg(new double[]{22.5,15.6,17.2}));
-        System.out.println(Alg.meanAvg(new double[]{1.0,1.0,1.0,50.0}));
-        System.out.println();
-        System.out.println(Alg.firstNeg(new int[]{1,3,-1,7,-3,2,4}));
-        System.out.println(Alg.firstNeg(new int[]{1,0,-1}));
-        System.out.println(Alg.firstNeg(new int[]{4,15,-122}));
-        System.out.println();
-        System.out.println(Alg.largestDif(new int[]{2,3,4},new int[]{-1,-2,-3}));
-        System.out.println(Alg.largestDif(new int[]{1,1,1},new int[]{-2,-6,-9}));
-        System.out.println(Alg.largestDif(new int[]{15,1,19919},new int[]{-2,-6,-9}));
-        System.out.println();
-        Alg.reverseArray(new int[]{1,2,3}); // prints to check if the change worked*
-        Alg.reverseArray(new int[]{4,2,3,1});
-        Alg.reverseArray(new int[]{99,88,77});
-        System.out.println();
-        System.out.println(Alg.twoConsecutiveVals(new int[]{1,2,3,3,4}));
-        System.out.println(Alg.twoConsecutiveVals(new int[]{3,3,3,3,3}));
-        System.out.println(Alg.twoConsecutiveVals(new int[]{5,4,3,2,1}));
-        System.out.println();
-        System.out.println(Alg.equivalantVals(new int[]{1,2,3,2}));
-        System.out.println(Alg.equivalantVals(new int[]{4,5,6}));
-        System.out.println(Alg.equivalantVals(new int[]{1,2,6,8,99999,4,5,99999}));
-        System.out.println();
-        System.out.println(Alg.longestStreak(new int[]{1,2,1,1,1},1));
-        System.out.println(Alg.longestStreak(new int[]{1,1,1,1,1,1,1,1},4));
-        System.out.println(Alg.longestStreak(new int[]{1,2,3,4,5},5));
-        System.out.println();
-        System.out.println(Alg.alphabeticallyLast(new String[]{"zebra","alpha"}));
-        System.out.println(Alg.alphabeticallyLast(new String[]{"Hamzah","Messi"}));
-        System.out.println(Alg.alphabeticallyLast(new String[]{"Trump","Biden"}));
-
-
-
-
-
-
-
-
-
-
-
-
+         Algorithm s = new Algorithm();
+        //LESSTHANZERO
+        System.out.println(s.lessThanZero(new int[]{1,0,-1}));
+        System.out.println(s.lessThanZero(new int[]{1,1,16}));
+        System.out.println(s.lessThanZero(new int[]{1,0,-1,-1,-1,-188,21}));
+        // ifFOUND
+        System.out.println(s.ifFound(new String[]{"string","apple"}, "string"));
+        System.out.println(s.ifFound(new String[]{"bat","apple"}, "string"));
+        System.out.println(s.ifFound(new String[]{"string","apple","cat","messi"}, "jholmer"));
+        // IN BOTH
+        System.out.println(s.inBoth(new ArrayList<Integer>(Arrays.asList(2,1)), new ArrayList<Integer>(Arrays.asList(2,3))));
+        System.out.println(s.inBoth(new ArrayList<Integer>(Arrays.asList(1,1)), new ArrayList<Integer>(Arrays.asList(2,15))));
+        System.out.println(s.inBoth(new ArrayList<Integer>(Arrays.asList(16,1,-1)), new ArrayList<Integer>(Arrays.asList(6,-1))));
+        // ARRAYLIST INDEX
+        System.out.println(s.firstIndexList(new ArrayList<Integer>(Arrays.asList(1,2,3)), 2));
+        System.out.println(s.firstIndexList(new ArrayList<Integer>(Arrays.asList(1,2,3,4,5)), 6));
+        System.out.println(s.firstIndexList(new ArrayList<Integer>(Arrays.asList(2,-2,-4)), 0));
+        // LIST INDEX
+        System.out.println(s.firstIndexArray(new int[]{1,2,3}, 2));
+        System.out.println(s.firstIndexArray(new int[]{4,5,6}, 6));
+        System.out.println(s.firstIndexArray(new int[]{0,0,0,0}, 9090909));
     }
-
-
-
-    }
-
+}
