9e3720c50c24ee9ff8f250dd1191d3d1ef17c19f
Hamzahk4 <110351211+Hamzahk4@users.noreply.github.com>
2024-02-26 14:52:36 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -23,14 +23,14 @@ public class Main {
     }
     public static void main(String[] args) {
         System.out.println("Arrays with selection sort:");
-        print(Algies.alphaSelection(new String[]{"Zebra", "Alpha"}));
+        print(Algies.alphaSelection(new String[]{"Zebra", "alpha"}));
         print(Algies.alphaSelection(new String[]{"Zebra", "Alpha", "Beta", "Woah", "Hi", "Hiiii"}));
-        print(Algies.alphaSelection(new String[]{"1", "2","4","6","3","9","2","5","h"}));
+        print(Algies.alphaSelection(new String[]{"1", "2","4","6","3","9","2","5","9"})); //tested to see if numbers as strings work (it does)
         System.out.println();
         System.out.println("ArrayLists with selection:");
-        printArrList(Algies.alphaSelection(new ArrayList<>(Arrays.asList("Zebra", "Alpha"))));
+        printArrList(Algies.alphaSelection(new ArrayList<>(Arrays.asList("Zebra", "alpha"))));
         printArrList(Algies.alphaSelection(new ArrayList<>(Arrays.asList("Zebra", "Alpha", "Beta", "Woah", "Hi", "Hiiii"))));
-        printArrList(Algies.alphaSelection(new ArrayList<>(Arrays.asList("1", "2","4","6","3","9","2","5","z"))));
+        printArrList(Algies.alphaSelection(new ArrayList<>(Arrays.asList("1", "2","4","6","3","9","2","5","9")))); //tested to see if numbers as strings work (it does)
         System.out.println("\nArrayLists of integers with selection sort:");
         printArrListOfInts(Algies.alphaSelectionInts(new ArrayList<>(Arrays.asList(1,0,-1))));
         printArrListOfInts(Algies.alphaSelectionInts(new ArrayList<>(Arrays.asList(1,0,-1,3,7,34,-4,-6))));
@@ -42,12 +42,12 @@ public class Main {
         System.out.println("\nArray with Insertion:");
         String[] str1 = {"z","b","a"}; String[] s1 = a.insertionSort(str1);
         String[] str2 = {"Zebra","alpha"}; String[] s2 = a.insertionSort(str2);
-        String[] str3 = {"cat","rat","mat", "jholmer"}; String[] s3 = a.insertionSort(str3);
+        String[] str3 = {"cat","rat","bat", "jholmer"}; String[] s3 = a.insertionSort(str3);
         print(s1); print(s2); print(s3);
         System.out.println("\nArrayList with Insertion:");
 //      // ArrayList with insertion sort
         ArrayList<String> aL1 = new ArrayList<String>(Arrays.asList("Zebra","alpha")); aL1 = a.insertionSortButForAL(aL1);
-        ArrayList<String> aL2 = new ArrayList<String>(Arrays.asList("Zebra","alpha","map","jholmer epic")); aL2 = a.insertionSortButForAL(aL2);
+        ArrayList<String> aL2 = new ArrayList<String>(Arrays.asList("Zebra","alpha","map","jholmer")); aL2 = a.insertionSortButForAL(aL2);
         ArrayList<String> aL3 = new ArrayList<String>(Arrays.asList("seeds","capes","urchin?")); aL3 = a.insertionSortButForAL(aL3);
         printArrList(aL1); printArrList(aL2); printArrList(aL3);
         System.out.println("\nArrayList of ints with Insertion:");
